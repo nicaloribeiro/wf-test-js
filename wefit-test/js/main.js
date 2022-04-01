ef9302c563bfb219394cb68a6f84fa22570f5735
Nícalo Ribeiro <nicalo63@gmail.com>
2022-03-31 22:23:28 -0300

    Script que realiza alteracoes no menu e no header

---
+++ wefit-test/js/main.js
@@ -0,0 +1,16 @@
+document.querySelector('.btn-group-vertical').classList.remove('btn-group-vertical');
+
+const headerDiv = document.querySelector('.jumbotron');
+headerDiv.style.alignItems = 'end'
+headerDiv.style.textAlign = 'right'
+headerDiv.style.color = 'white'
+headerDiv.style.backgroundColor = '#6B757E'
+
+const headerButton = headerDiv.querySelector('a')
+headerButton.style.backgroundColor = '#27A844'
+headerButton.style.border = 'none'
+
+
+
+
+
