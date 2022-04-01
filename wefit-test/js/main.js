ff6cecf12488fbf4803e77506396ead87af1518a
Nícalo Ribeiro <nicalo63@gmail.com>
2022-03-31 23:18:44 -0300

    Script que realiza alteracoes nos cards e na lista

--- wefit-test/js/main.js
+++ wefit-test/js/main.js
@@ -1,6 +1,6 @@
-document.querySelector('.btn-group-vertical').classList.remove('btn-group-vertical');
+document.querySelector('.btn-group-vertical').classList.remove('btn-group-vertical')
 
-const headerDiv = document.querySelector('.jumbotron');
+const headerDiv = document.querySelector('.jumbotron')
 headerDiv.style.alignItems = 'end'
 headerDiv.style.textAlign = 'right'
 headerDiv.style.color = 'white'
@@ -10,7 +10,29 @@ const headerButton = headerDiv.querySelector('a')
 headerButton.style.backgroundColor = '#27A844'
 headerButton.style.border = 'none'
 
+const cardsDiv = document.getElementsByClassName('row')[2]
 
+const cardAnimais = cardsDiv.children[1]
+const cardTecnologias = cardsDiv.children[2]
+const cardPessoas = cardsDiv.children[3]
+const cardNatureza = cardsDiv.children[4]
 
+const cardAnimaisButton = cardsDiv.querySelector('a')
+cardAnimaisButton.style.backgroundColor = '#27A844'
+cardAnimaisButton.style.border = 'none'
 
+const newCardOrder = [cardsDiv.children[0], cardNatureza, cardAnimais, cardPessoas, cardTecnologias]
+cardsDiv.replaceChildren(...newCardOrder)
 
+const listElement = document.querySelector('ul')
+listElement.children[0].classList.remove('active')
+
+const quartoItem = document.createElement('li')
+quartoItem.classList.add('list-group-item', 'active')
+quartoItem.appendChild(document.createTextNode('Quarto item'))
+listElement.appendChild(quartoItem)
+
+const quintoItem = document.createElement('li')
+quintoItem.classList.add('list-group-item')
+quintoItem.appendChild(document.createTextNode('Quinto item'))
+listElement.appendChild(quintoItem)
